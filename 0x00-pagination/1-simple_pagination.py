7f160ea94a3827d733c3c9cdaf8e4803848e329f
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-20 15:01:47 +0000

    simple they said

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
-"""Simple pagination with a Server class"""
-
+"""Module to paginate items from a dataset of popular baby names."""
 import csv
-from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple containing a start index and an end index."""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+from typing import List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names"""
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Load and cache the dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,15 +21,19 @@ class Server:
 
         return self.__dataset
 
+    @staticmethod
+    def index_range(page: int, page_size: int) -> tuple:
+        """Calculate the start and end indexes for the pagination parameters"""
+        start = (page - 1) * page_size
+        end = page * page_size
+        return start, end
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a page of the dataset."""
+        """Return a page of items from the dataset."""
         assert isinstance(page, int) and page > 0,
         assert isinstance(page_size, int) and page_size > 0,
 
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-
-        if start_index >= len(dataset):
+        start, end = self.index_range(page, page_size)
+        if start >= len(self.dataset()):
             return []
-
-        return dataset[start_index:end_index]
+        return self.dataset()[start:end]
