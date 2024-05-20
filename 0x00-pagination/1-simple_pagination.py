b7df93a7d8442fefc0840da4330977472e538476
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-20 15:04:04 +0000

    simple they said

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-"""Module to paginate items from a dataset of popular baby names."""
+"""Module to paginate items from a dataset of popular baby names"""
+
 import csv
+import math
 from typing import List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Paginate a database"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Load and cache the dataset"""
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -22,18 +24,18 @@ class Server:
         return self.__dataset
 
     @staticmethod
-    def index_range(page: int, page_size: int) -> tuple:
-        """Calculate the start and end indexes for the pagination parameters"""
+    def index_range(page, page_size) -> tuple:
+        """idx"""
         start = (page - 1) * page_size
         end = page * page_size
-        return start, end
+        return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a page of items from the dataset."""
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size, int) and page_size > 0,
+        """Get"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start, end = self.index_range(page, page_size)
-        if start >= len(self.dataset()):
+        if (page, page_size) > (start, end):
             return []
         return self.dataset()[start:end]
