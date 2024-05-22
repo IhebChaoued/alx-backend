228f79edfa9d3b75323e5f107b27ef2abed91d79
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-22 19:03:33 +0000

    lifo?

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ LIFO caching module
 """
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFOCache defines a caching system with LIFO eviction policy """
+    """LIFOCache defines a caching system"""
 
     def __init__(self):
         """ Initialize the cache """
@@ -13,19 +13,24 @@ class LIFOCache(BaseCaching):
         self.last_key = None
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ Get an item by key """
         if key is None or item is None:
             return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            if self.last_key is not None:
+                print("DISCARD:", self.last_key)
+                del self.cache_data[self.last_key]
 
         self.cache_data[key] = item
         self.last_key = key
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            del self.cache_data[self.last_key]
-            print(f"DISCARD: {self.last_key}")
-
     def get(self, key):
         """ Get an item by key """
-        if key is None:
+        if key is None or key not in self.cache_data:
             return None
         return self.cache_data.get(key)
+
+
+if __name__ == "__main__":
+    LIFOCache = LIFOCache
