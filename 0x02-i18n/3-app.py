6705f77089957504e550e47534234d357a07b992
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-28 21:46:05 +0000

    up

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,7 +3,7 @@
 Flask app with Babel setup and translations
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
