648dfb6ceb1b504f3753914bf7e992410bfe4096
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-28 21:31:34 +0000

    updated

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app with Babel setup and translations"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
