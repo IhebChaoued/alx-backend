b9565ff7610950a5d966d335bbb8c33ad84e5e99
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-28 22:11:03 +0000

    Mock user login

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,8 +4,16 @@ from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
-class Config:
-    """ app config """
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """Config"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,29 +24,22 @@ babel = Babel(app)
 
 app.config.from_object(Config)
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 def get_user(user_id):
-    """ Get user by ID """
+    """Get user by ID"""
     return users.get(user_id)
 
 
 @app.before_request
 def before_request():
-    """ Set current user """
+    """Set current user"""
     user_id = request.args.get('login_as')
     g.user = get_user(int(user_id)) if user_id else None
 
 
 @app.route("/")
-def index():
-    """ index.html """
+def index() -> str:
+    """index.html"""
     return render_template("5-index.html")
 
 
