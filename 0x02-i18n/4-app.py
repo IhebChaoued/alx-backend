aa1af274566c1a4bc5a319625500d4138636fb80
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-28 21:30:12 +0000

    updated

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Babel setup, translations, and locale selection"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
